de8abf2417586555fcc4f0df9b4d2a1b8f8a53c3
league <league@imac-22.attlocal.net>
2022-05-13 16:53:16 -0700

    this iss done, do socrative next week

--- src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
+++ src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
@@ -5,11 +5,24 @@ import javax.swing.JOptionPane;
 public class RollerCoaster {
 
 	// 1) Make a main method that includes all the steps below…. 
-		
+		public static void main(String[] args) {
+			
 		// 2) Ask the user to enter their height in inches using JOptionPane and set it to the variable heightToRide
-		
+	String heightToRide =	JOptionPane.showInputDialog(null,"whats your height (in inches)");
+	
+	int heightasInt = Integer.parseInt(heightToRide);
 		// Uncomment the line below...
-			//if( heightToRide > 42 ) { }
+	if(heightasInt > 48){
+		JOptionPane.showMessageDialog(null, "you can ride alone.");
+	}
+			if( heightasInt > 46 ) {
+				JOptionPane.showMessageDialog(null, "you can ride with someone else");
+			} else if(heightasInt > 42 ){
+				JOptionPane.showMessageDialog(null,"you can't ride at all (and your short...)");
+			}
+		}
+}
+			
 		
 		// Do you see the heightToRide > 42 underlined red? That is because you are comparing a STRING to an INTEGER.
 		
@@ -22,4 +35,6 @@ public class RollerCoaster {
 		// "You can ride the coaster with someone else!" 
 	
 		// 6) else print: "You must be at least 42 inches tall to ride the roller coaster pal!"
-} 
+
+
+
--- src/_04_spot_the_error/MethodCall.java
+++ src/_04_spot_the_error/MethodCall.java
@@ -7,10 +7,13 @@ public class MethodCall {
 	// Add ONE line of code to fix this program.
 	
 	public static void main(String[] args) {
-		String aMessage = hello world;
+		String aMessage = "hello world";
+		printMessage(aMessage);
 	}
 	
+
 	static void printMessage(String aMessage) {
+
 		System.out.println(aMessage);
 	}
 }
